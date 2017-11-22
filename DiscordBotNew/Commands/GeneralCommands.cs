0c8e3a41da705fcf3c0e3552c46319cfed404a0c
Aaron Opell <aaron.opell@live.com>
2017-11-22 14:25:28 -0800

    Condensed decide into one function instead of two

--- DiscordBotNew/Commands/GeneralCommands.cs
+++ DiscordBotNew/Commands/GeneralCommands.cs
@@ -489,11 +489,8 @@ namespace DiscordBotNew.Commands
         [Command("ping"), HelpText("Displays the bot's latency")]
         public static ICommandResult Ping(ICommandContext context) => new SuccessResult($"{context.Bot.Client.Latency} ms estimated round trip latency");
 
-        [Command("decide"), HelpText("Replies with 'yes' or 'no' (or always `option` if provided) to prevent indecisiveness")]
-        public static ICommandResult Decide(ICommandContext context, [JoinRemainingParameters] string option = null) => new SuccessResult(option ?? (random.Next(2) == 0 ? "Yes" : "No"));
-
         [Command("decide"), HelpText("Picks a random option")]
-        public static ICommandResult Decide(ICommandContext context, string option1, string option2, [JoinRemainingParameters, HelpText("Additional space-separated options")] string[] otherOptions = null) => new SuccessResult((otherOptions ?? new string[0]).Append(option1).Append(option2).OrderBy(x=>random.Next()).First());
+        public static ICommandResult Decide(ICommandContext context, [JoinRemainingParameters] string[] options = null) => new SuccessResult(options?[random.Next(options.Length)] ?? (random.Next(2) == 0 ? "Yes" : "No"));
 
         [Command("timestamp"), HelpText("Displays the timestamp of a message"), CommandScope(ChannelType.Text)]
         public static async Task<ICommandResult> Timestamp(DiscordMessageContext context, [DisplayName("message ID")] ulong messageId, [DisplayName("channel mention")] string channel, [DisplayName("Windows TimeZone ID")] string timeZone = "Pacific Standard Time")
