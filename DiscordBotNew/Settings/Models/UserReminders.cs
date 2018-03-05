9f5a48c1a836cb6b804f338546e95e86ac41f928
Aaron Opell <aaron.opell@live.com>
2018-03-04 16:23:36 -0800

    Only store user info in footer

--- DiscordBotNew/Settings/Models/UserReminders.cs
+++ DiscordBotNew/Settings/Models/UserReminders.cs
@@ -40,30 +40,18 @@ namespace DiscordBotNew.Settings.Models
             set => Message = value;
         }
 
-        public ReminderSlim Slim() => this;
+        public ReminderUserInfoSlim Slim() => this;
     }
 
-    public class ReminderSlim
+    public class ReminderUserInfoSlim
     {
         public ulong S { get; set; }
         public ulong R { get; set; }
-        public DateTimeOffset T { get; set; }
-        public string M { get; set; }
 
-        public static implicit operator ReminderSlim(ReminderInfo r) => new ReminderSlim
+        public static implicit operator ReminderUserInfoSlim(ReminderInfo r) => new ReminderUserInfoSlim
         {
             S = r.SenderId,
-            R = r.ReceiverId,
-            T = r.Timestamp,
-            M = r.Message
-        };
-
-        public static implicit operator ReminderInfo(ReminderSlim r) => new ReminderInfo
-        {
-            SenderId = r.S,
-            ReceiverId = r.R,
-            Timestamp = r.T,
-            Message = r.M
+            R = r.ReceiverId
         };
     }
 }
